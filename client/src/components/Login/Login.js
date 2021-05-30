b48c43520fbd1ee1831216b5850b7a3f373fb6c7
bkaradz <karadz@gmail.com>
2021-05-30 11:04:49 +0200

    Login form

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,64 +1,37 @@
-import { InputGroup, DropdownButton, Dropdown, FormControl, Button } from 'react-bootstrap-v5'
-import { BiUser } from 'react-icons/bi'
+import React, { useState } from 'react'
+
+import { Modal, Button } from 'react-bootstrap-v5'
+// import { BiUser } from 'react-icons/bi'
 
 function Login() {
+  const [modalShow, setModalShow] = useState(false)
+
+  function MyVerticallyCenteredModal(props) {
+    return (
+      <Modal {...props} size='lg' aria-labelledby='contained-modal-title-vcenter' centered>
+        <Modal.Header closeButton>
+          <Modal.Title id='contained-modal-title-vcenter'>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>Centered Modal</h4>
+          <p>
+            Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+            consectetur ac, vestibulum at eros.
+          </p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
   return (
     <div className='main'>
-      <InputGroup className='my-3 input-group-sm'>
-        <DropdownButton variant='outline-secondary' title='Dropdown' id='input-group-dropdown-1'>
-          <Dropdown.Item href='#'>Action</Dropdown.Item>
-          <Dropdown.Item href='#'>Another action</Dropdown.Item>
-          <Dropdown.Item href='#'>Something else here</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href='#'>Separated link</Dropdown.Item>
-        </DropdownButton>
-        <FormControl aria-label='Text input with dropdown button' />
-      </InputGroup>
+      <Button variant='primary' onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
 
-      <InputGroup className='mb-3'>
-        <FormControl aria-label='Text input with dropdown button' />
-
-        <DropdownButton variant='outline-secondary' title='Dropdown' id='input-group-dropdown-2' align='end'>
-          <Dropdown.Item href='#'>Action</Dropdown.Item>
-          <Dropdown.Item href='#'>Another action</Dropdown.Item>
-          <Dropdown.Item href='#'>Something else here</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href='#'>Separated link</Dropdown.Item>
-        </DropdownButton>
-      </InputGroup>
-
-      <InputGroup className='mb-3'>
-        <DropdownButton variant='outline-secondary' title='Dropdown' id='input-group-dropdown-3'>
-          <Dropdown.Item href='#'>Action</Dropdown.Item>
-          <Dropdown.Item href='#'>Another action</Dropdown.Item>
-          <Dropdown.Item href='#'>Something else here</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href='#'>Separated link</Dropdown.Item>
-        </DropdownButton>
-        <FormControl aria-label='Text input with 2 dropdown buttons' />
-        <DropdownButton variant='outline-secondary' title='Dropdown' id='input-group-dropdown-4' align='end'>
-          <Dropdown.Item href='#'>Action</Dropdown.Item>
-          <Dropdown.Item href='#'>Another action</Dropdown.Item>
-          <Dropdown.Item href='#'>Something else here</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href='#'>Separated link</Dropdown.Item>
-        </DropdownButton>
-      </InputGroup>
-
-      <InputGroup className='mb-3'>
-        <DropdownButton variant='outline-primary' title='Dropdown' id='input-group-dropdown-3'>
-          <Dropdown.Item href='#'>Action</Dropdown.Item>
-          <Dropdown.Item href='#'>Another action</Dropdown.Item>
-          <Dropdown.Item href='#'>Something else here</Dropdown.Item>
-          <Dropdown.Divider />
-          <Dropdown.Item href='#'>Separated link</Dropdown.Item>
-        </DropdownButton>
-        <FormControl aria-label='Text input with 2 dropdown buttons' />
-        <Button type='button' className='btn btn-outline-secondary btn-secondary' id='input-group-dropdown-4' align='end'>
-          {' '}
-          <BiUser />{' '}
-        </Button>
-      </InputGroup>
+      <MyVerticallyCenteredModal show={modalShow} onHide={() => setModalShow(false)} />
     </div>
   )
 }
