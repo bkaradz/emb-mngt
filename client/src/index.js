eaa2b2ebb197cea7597797ede02ff291f695f7c8
bkaradz <karadz@gmail.com>
2021-06-21 09:20:36 +0200

    branch to convert to material ui

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import { ThemeProvider } from '@material-ui/core/styles'
+// import theme from 'src/theme'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+  //  theme={theme}
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>,
+  document.getElementById('root')
+)
