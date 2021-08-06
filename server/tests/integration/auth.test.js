cd4b55a26a84a9c7583fb6e72c419e4934e0418a
bkaradz <karadz@gmail.com>
2021-08-06 15:35:51 +0200

    Added

--- server/tests/integration/auth.test.js
+++ server/tests/integration/auth.test.js
@@ -2,6 +2,9 @@ const request = require('supertest')
 const Users = require('../../models/Users')
 let server
 
+/**
+ * Testing POST /api/auth
+ */
 describe('/api/auth', () => {
   beforeEach(() => {
     server = require('../../server')
@@ -11,11 +14,8 @@ describe('/api/auth', () => {
     await Users.deleteMany({})
   })
   describe('POST /', () => {
-    // Add User
     it('should return a user', async () => {
-      // await Users.collection.insertMany([
-      //   ,
-      // ])
+      // Add User
       const resCreate = await request(server).post('/api/users').send({
         name: 'John Doe',
         email: 'johndoe@gmail.com',
@@ -25,11 +25,45 @@ describe('/api/auth', () => {
         password2: 'john123',
       })
 
-      console.log(resCreate.body)
-
       const res = await request(server).post('/api/auth').send({ email: 'johndoe@gmail.com', password: 'john123' })
-      console.log(res.body)
+
       expect(res.status).toBe(200)
+      expect(res.body.token).toBeTruthy()
+    })
+
+    it('should throw if email is falsy or is not valid', async () => {
+      const args = [
+        null,
+        '',
+        undefined,
+        NaN,
+        0,
+        false,
+        'gone',
+        'example.com',
+        'A@b@c@domain.com',
+        'a”b(c)d,e:f;gi[jk]l@domain.com',
+        'abc”test”email@domain.com',
+        'abc is”not\valid@domain.com',
+        'abc is”not\valid@domain.com',
+        '.test@domain.com',
+        'test@domain..com',
+      ]
+
+      args.forEach((a) => {
+        expect(async () => {
+          await request(server).post('/api/auth').send({ email: a, password: 'john123' }).toThrow()
+        })
+      })
+    })
+    it('should throw if password is falsy or is less than 6 characters', async () => {
+      const args = [null, '', undefined, NaN, 0, false, 'gone']
+
+      args.forEach((a) => {
+        expect(async () => {
+          await request(server).post('/api/auth').send({ email: 'johndoe@gmail.com', password: a }).toThrow()
+        })
+      })
     })
   })
 })
