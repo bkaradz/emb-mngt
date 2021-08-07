ace19f7bf9b667e39e3e50b5420ba9f58d453bb9
bkaradz <karadz@gmail.com>
2021-08-07 17:02:51 +0200

    working on users tests

--- server/tests/integration/users.test.js
+++ server/tests/integration/users.test.js
@@ -13,35 +13,43 @@ describe('/api/users', () => {
   describe('GET /', () => {
     let token
     it('should return a user', async () => {
+      const resCreate = await request(server).post('/api/users').send({
+        name: 'John Doe',
+        email: 'johndoe@gmail.com',
+        role: 'admin',
+        mobile: '0789123456',
+        password: 'john123',
+        password2: 'john123',
+      })
       // Add Users
       await Users.collection.insertMany([
-        {
-          name: 'John Doe',
-          email: 'johndoe@gmail.com',
-          role: 'admin',
-          mobile: '0789123456',
-          password: 'john123',
-          password2: 'john123',
-        },
         {
           name: 'Jane Doe',
           email: 'janedoe@gmail.com',
           role: 'sales',
           mobile: '0789123456',
           password: 'jane123',
-          password2: 'jane123',
+        },
+        {
+          name: 'john juba',
+          email: 'johnjuba@gmail.com',
+          role: 'sales',
+          mobile: '0789123456',
+          password: 'juba123',
         },
       ])
 
       const resToken = await request(server).post('/api/auth').send({ email: 'johndoe@gmail.com', password: 'john123' })
 
+      // console.log(resToken.body)
       token = resToken.body.token
 
       const res = await request(server).get('/api/users').set({ 'x-auth-token': token })
 
       // console.log(res.body)
       expect(res.status).toBe(200)
-      expect(res.body.email).toContain('johndoe@gmail.com')
+      expect(res.body.length).toBe(3)
+      // expect(res.body.email).toContain('johndoe@gmail.com')
     })
   })
 })
